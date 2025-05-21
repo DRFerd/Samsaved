761b6ca61b0d071783e1c27d1f242e4a2ab9d143
Ferdinand <chiemeriennadi60@gmail.com>
2025-05-21 18:42:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// document.getElementById("myH1").textContent = "Hello Beautiful Word";
+//document.getElementById("myP").textContent = "My Name is Ferdinand the best enterprenuer and basketbaall of all time";
+//window.alert("This is to inform you that you are reading what Ferdinadn made & created.")
+
+/*var myDog = {
+    'legs': 4,
+    'tails': 1,
+    'skin': ["black && yellow"],
+    "friends": ['many'],
+};
+console.log('legs');*/
+
+/*let fodd = 20;
+let water = 10;
+console.log{fodd};*/
+
+
+
+  // const input = document.getElementById('input-type');
+
+  // input.addEventListener('focus', async () => {
+  //   try {
+  //     const text = await navigator.clipboard.readText();
+  //     input.value = text;
+
+  //     // Optional: blur after pasting to hide keyboard on mobile
+  //     input.blur();
+  //   } catch (err) {
+  //     alert("Unable to paste. Please allow clipboard permissions.");
+  //   }
+  // });
+
+
+
